73c118c80e242bd8e47e086543b9275f37d2c279
Andrew Foltz-Morrison <thefomo@gmail.com>
2024-07-14 12:35:12 -0400

    CSS fixes for variable fonts and initial def'd symbol highlighting

--- dev/site.clj
+++ dev/site.clj
@@ -17,7 +17,7 @@
    [:link
     {:rel "stylesheet"
      :href
-     "https://fonts.googleapis.com/css2?family=Eczar:wght@400..800&family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800&family=Recursive:slnt,wght,CASL,CRSV,MONO@-15..0,300..1000,0..1,0..1,0..1&display=swap"}]
+     "https://fonts.googleapis.com/css2?family=Newsreader:ital,opsz,wght@0,6..72,200..800;1,6..72,200..800"}]
    [:link
     {:href "https://unpkg.com/normalize.css@8.0.1/normalize.css"
      :rel  "stylesheet"}]
